9d097a9ff3fa9dbe95cbd9e9712be664f6c4bfb3
Samuelalfonso <samuelalfonzoc@hotmail.com>
2016-05-18 14:53:57 -0430

    Update ejercicio1.py
    Mensajes por consola .

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,12 @@
+
 # prueba1.1
 
-print "hola mundo"
+print ("Dime tu nombre")
+Nombre= input()
+
+print ("El nombre que has introducido es:",Nombre)
+
+Edad= input("Y ahora dime tu edad >")
+edadnumero= int (edad)
+
+print("El doble de tu edad es %d"(edadnumero*3))
